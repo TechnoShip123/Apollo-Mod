a932095997862ee4557cbd44f2461ea636b8e871
TechnoShip123 <sopan.jalkote@gmail.com>
2020-12-12 12:31:51 -0600

    See the updates on Issue #3 for more information, but basically I've refactored the files and accidentally left @mod to be `examplemod`, which is somehow working and well, Herobrine knows why, so I'm committing this before changing it to also be `apollo` or `apollomod`.

--- src/main/java/com/technoship/apollo/ApolloMod.java
+++ src/main/java/com/technoship/apollo/ApolloMod.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("examplemod") // I mean this should be changed later, somehow it works now so let's try making it apollomod or apollo.
 public class ApolloMod
 {
     // Directly reference a log4j logger.`
