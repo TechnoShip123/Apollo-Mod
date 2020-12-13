d2f244c9c54f5941d9896fcc626fa1620fc011d3
TechnoShip123 <sopan.jalkote@gmail.com>
2020-12-12 22:08:42 -0600

    Added the Creative Inventory tab for the mod! Included the ruby icon as the symbol, see the goal #1 for more details.

--- src/main/java/com/technoship/apollomod/ApolloMod.java
+++ src/main/java/com/technoship/apollomod/ApolloMod.java
@@ -1,6 +1,8 @@
 package com.technoship.apollomod;
 
 import com.technoship.apollomod.util.RegistryHandler;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -30,4 +32,11 @@ public class ApolloMod
     private void setup(final FMLCommonSetupEvent event) {    }
 
     private void doClientStuff(final FMLClientSetupEvent event) {    }
+
+    public static final ItemGroup TAB = new ItemGroup("apolloTab") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(RegistryHandler.RUBY.get());
+        }
+    };
 }
