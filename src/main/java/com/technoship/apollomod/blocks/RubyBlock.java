d3f4cdb170b83f7d6db0b04181ba6243ee22d41f
TechnoShip123 <sopan.jalkote@gmail.com>
2020-12-13 08:55:09 -0600

    Fixed the error stopping the ruby block from getting added in game, as well as adding the ruby block in game. You can now find it and mine it with the `.harvestlevel` and such that I have added in the code. See goal #6 for more details.

--- src/main/java/com/technoship/apollomod/blocks/RubyBlock.java
+++ src/main/java/com/technoship/apollomod/blocks/RubyBlock.java
@@ -5,15 +5,16 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.common.ToolType;
 
-public class RubyBlock extends Block {
-    public RubyBlock(Properties properties) {
+public class RubyBlock extends Block{
+
+    public RubyBlock() {
+
         super(Block.Properties.create(Material.IRON)
-                .hardnessAndResistance(4.0f,5.0f)
-                .sound(SoundType.METAL) //Experiment with different sounds(?)
-                .harvestLevel(2) //0 = wooden, 1 = stone/gold, 2 = iron, 3 = diamond. This is the level of tool required to pick up the block.
-                .harvestTool(ToolType.PICKAXE) //Set the tool used to mine the block.
+                .hardnessAndResistance(4.0f,6.0f)
+                .sound(SoundType.METAL)
+                .harvestLevel(2)
                 .lightValue(2)
-                .jumpFactor(8) //TODO: Remove this later
-        );
+                .harvestTool(ToolType.PICKAXE));
+
     }
 }
